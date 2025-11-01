6c151450873d211a3f73b783fa42fe670ba2e850
Joel Hediger <joel.hediger@sonnenberg-baar.ch>
2025-11-02 00:48:32 +0100

    Mobile Optimierung: Übersicht-Seite angepasst

--- pages/shop.php
+++ pages/shop.php
@@ -200,16 +200,10 @@ if ($category_id !== '') {
                                                 title="Zur Merkliste hinzufügen">
                                             <?php echo get_icon('heart', 20); ?>
                                         </button>
-                                        <button class="btn-add-cart-mobile"
-                                                onclick="event.preventDefault(); event.stopPropagation(); addToCartFromCard('<?php echo $wine['id']; ?>', '<?php echo addslashes($wine['name']); ?>', <?php echo $wine['price']; ?>);"
-                                                title="In Warenkorb">
-                                            <?php echo get_icon('shopping-cart', 18); ?>
-                                            <span>In Warenkorb</span>
-                                        </button>
                                     </div>
                                 </div>
 
-                                <!-- Mengenauswahl -->
+                                <!-- Mengenauswahl & In Warenkorb -->
                                 <div class="wine-quantity-section" onclick="event.preventDefault(); event.stopPropagation();">
                                     <div class="quantity-selector-compact">
                                         <button class="qty-btn" onclick="decreaseQty('<?php echo $wine['id']; ?>')">
@@ -226,7 +220,7 @@ if ($category_id !== '') {
                                             <?php echo get_icon('plus', 16); ?>
                                         </button>
                                     </div>
-                                    <button class="btn-add-cart-desktop"
+                                    <button class="btn-add-cart"
                                             onclick="addToCartFromCard('<?php echo $wine['id']; ?>', '<?php echo addslashes($wine['name']); ?>', <?php echo $wine['price']; ?>)">
                                         <?php echo get_icon('shopping-cart', 18); ?>
                                         <span>In Warenkorb</span>
